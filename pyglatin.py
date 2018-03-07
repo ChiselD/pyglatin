b413ac540c2c0b1ffbaae8d11fea10085105d371
Jinx <chisel.delahanty@gmail.com>
2018-03-07 22:32:49 +0100

    update ending variables and input prompt

--- pyglatin.py
+++ pyglatin.py
@@ -1,8 +1,11 @@
-# create separate variable for ending
-pyg = 'ay'
+# two categories of words: starting with vowels and starting with consonants
 
-# prompt user for word. TODO: change to full phrase
-original = raw_input('Enter a word: ')
+# create separate variable for ending
+ay = "ay"
+yay = "yay"
+
+# prompt user for text
+original = raw_input("Enter your text: ")
 
 # check if string is not empty and is purely alphabetical
 if len(original) > 0 and original.isalpha():
@@ -11,7 +14,7 @@ if len(original) > 0 and original.isalpha():
   # make variable to hold first letter only
   first = word[0]
   # PL-ized word = all-but-first of original + first of original + 'ay'
-  new_word = word[1:len(word)] + first + pyg
+  new_word = word[1:len(word)] + first + ay
   print new_word
 else:
   print 'empty'
