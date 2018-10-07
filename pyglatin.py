bb84cd9ad4de8ae5bd6e59138f9eb85672d9816f
Jinx <chisel.delahanty@gmail.com>
2018-10-07 19:36:51 +0200

    refactor and change uickqay to ickquay

--- pyglatin.py
+++ pyglatin.py
@@ -2,16 +2,24 @@
 # 1. multiple consonants at start of word - DONE!
 # 2. printing on separate lines - DONE!
 # 3. non-alphabetical strings
-# 3a. if user includes numbers, return error message saying "only letters & punctuation please"
+# 3a. if user includes numbers, return error - DONE!
 # 3b. if user includes punctuation, move it to correct location
 # 4. omitted capitalization
 
 # create separate variables for the two possible endings
-ay = "ay"
-yay = "yay"
+ay = 'ay'
+yay = 'yay'
 
 # create list to hold words of sentence in order
 sentence = []
+
+# create reference list that tracks all vowels
+vowels = ['a','e','i','o','u','y']
+
+# define function to check if user input contains any digits
+def has_number(input):
+	# returns True if the string contains a digit
+	return any(char.isdigit() for char in input)
 
 # define function to run on words starting with vowels
 def vowel(word):
@@ -21,14 +29,15 @@ def vowel(word):
 # define function to run on words starting with consonants
 def consonant(word, first):
 	# create variable to hold all letters before first vowel
-	first_chunk = ""
+	first_chunk = ''
 	# I don't think there are any words in English that start with 'y' + another consonant (?)
-	if first == "y":
+	if first == 'y':
 		first_chunk = first
+	elif word[0] == 'q' and word[1] == 'u':
+		first_chunk = 'qu'
 	else:
 		for letter in word:
 			# look at each letter in word to see if it is a vowel
-			vowels = ['a','e','i','o','u','y']
 			if letter not in vowels:
 				# as long as it's not, add it onto the end of the 'first_chunk' variable
 				first_chunk += letter
@@ -43,8 +52,12 @@ def main():
 	# prompt user for text to Pig-Latinize
 	original = raw_input("Enter your text: ")
 
+	if has_number(original) == True:
+		print "Please enter a string that contains only letters and punctuation."
+		main()
+
 	# confirm that user-entered string is not empty
-	if len(original) > 0:
+	elif len(original) > 0:
 		# lowercase user-entered string for practical purposes
 		text = original.lower()
 		# split original text into array of separate words
@@ -54,7 +67,7 @@ def main():
 			# make variable to hold first letter only
 			first = word[0]
 			# if first letter is vowel, run vowel function
-			if first == "a" or first == "e" or first == "i" or first == "o" or first == "u":
+			if first in vowels and first != 'y':
 				vowel(word)
 			# if first letter is consonant, run consonant function
 			else:
@@ -65,6 +78,7 @@ def main():
 			print item,
 
 	else:
-		print 'empty'
+		print "No input!"
+		main()
 
 main()
